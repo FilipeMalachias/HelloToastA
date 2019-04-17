0e054ffe9a172113686f8a6c2c37ab911b6e5c46
FilipeMalachias <Filipe_malachias@hotmail.com>
2019-04-17 11:18:34 -0700

    Make MainActivity

---
+++ app/src/main/java/com/filipe/malachias/hello_toast/MainActivity.java
@@ -0,0 +1,12 @@
+package com.filipe.malachias.hello_toast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
